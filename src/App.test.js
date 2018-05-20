e05fdd1d62bea167c41935a16682eace52b283c9
abdallahkawji <abdallah@styla.com>
2018-05-20 19:05:40 +0200

    refactored boilerplate code structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SqueezeMotions from './SqueezeMotions';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SqueezeMotions />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SqueezeMotions from './SqueezeMotions';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SqueezeMotions />, document.getElementById('root'));
 registerServiceWorker();
